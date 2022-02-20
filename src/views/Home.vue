f6418100bef605654ed188165270cfb230d78a60
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-02-20 16:04:45 +0300

    Show data pulled from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,36 @@
 
 <script>
 import ValuesCard from '@/components/ValuesCard'
+import { mapActions } from 'vuex'
 export default {
   name: 'Home',
+  data () {
+    return {
+      global: {}
+    }
+  },
   components: {
     ValuesCard
+  },
+  async mounted () {
+    const summary = await this.fetchSummary()
+    this.global = summary.Global
+    const date = new Date()
+    this.global.Date = date.toLocaleString('en-US',{
+      weekday: 'long',
+      day:'numeric',
+      year:'numeric'
+    })
+  },
+  methods: {
+    ...mapActions(['fetchSummary'])
   }
 }
 </script>
+
 <template>
   <div class="home">
-    <values-card></values-card>
+    <values-card v-bind:global='this.global'></values-card>
   </div>
 </template>
 
