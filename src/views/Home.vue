55a8eac226285c0c9639e8172729fe3abfd1c8e5
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-02-24 22:08:27 +0300

    Fix the appearance of the Map chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,20 @@ export default {
 
 <template>
   <div class="home">
-    <map-chart :countriesData="countriesData"></map-chart>
+    <map-chart class="map" :countriesData="countriesData"></map-chart>
   </div>
 </template>
 
+<style lang="scss">
+.home {
+  margin: 50px auto;
+}
+.map {
+  max-width: 950px;
+  box-shadow: 0 0 10px rgb(202, 202, 202);
+  margin: 0 auto;
+  padding:10px;
+  background-color: white;
+  border-radius: 8px;
+}
+</style>
