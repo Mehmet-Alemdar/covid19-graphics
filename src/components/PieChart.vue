f62b9d1873ae8a1263a978d80f80bb7b878eea3a
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-02-27 10:55:39 +0300

    Create a pie chart component

---
+++ src/components/PieChart.vue
@@ -0,0 +1,49 @@
+<script>
+  import { Pie } from 'vue-chartjs'
+  import { mapActions } from 'vuex'
+  export default {
+    extends: Pie,
+    props: {
+      data: []
+    },
+    data() {
+      return {
+        labelsOne: [],
+        values: Number,
+        valuesT: Number,
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          sclales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      }
+    },
+    async mounted() {
+      let chartData = {
+        labels: this.data.label,
+        datasets: [{
+          borderWidth: 1,
+          borderColor: [
+            'rgba(0, 229, 217,1)',
+            'rgba(60, 195, 0, 1)'
+            ],
+          backgroundColor: [
+            'rgba(0, 229, 217, 0.5)',
+            'rgba(60, 195, 0, 0.5)'          
+          ],
+          data: this.data.value
+        }]
+      }
+      this.renderChart(chartData, this.options)
+    },
+    methods: {
+      ...mapActions(['fetchSummary'])
+    }
+  }
+</script>
