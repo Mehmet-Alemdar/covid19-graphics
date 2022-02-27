7e65b27fb025f332cc87293fc2383b978d94cacd
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-02-27 10:57:05 +0300

    Show data with pie chart on country page

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,5 +1,6 @@
 <script>
 import { mapActions } from 'vuex';
+import PieChart from '@/components/PieChart'
 export default {
   name: 'Country',
   data () {
@@ -8,6 +9,7 @@ export default {
     }
   },
   components: {
+    PieChart
   },
   async mounted () {
     const countries = await this.fetchSummary()
@@ -28,6 +30,7 @@ export default {
       <p>Active: {{country.active}}</p>
       <p>Critical: {{country.critical}}</p>
     </div>
+    <a href="#pie-charts">Go To Pie Charts</a>
     <div class="comparison-container">
       <div class="comparison"><p class="value">Today Deaths <p>{{country.todayDeaths}}</p></p></div>
       <div class="comparison"><p class="value">Deaths <p >{{country.deaths}}</p> </p></div>
@@ -36,15 +39,70 @@ export default {
       <div class="comparison"><p class="value">Today Recovered <p >{{country.todayRecovered}}</p> </p></div>
       <div class="comparison"><p class="value">Recovered <p >{{country.recovered}}</p> </p></div>      
     </div>
+    <div v-if="country" id="pie-charts">
+      <div  class="pie-chart-container">
+        <h2 class="comparison-title">Deaths</h2>
+        <pie-chart class="pie-chart" :data='{label: ["Deaths","Today Deaths"], value:[country.deaths,country.todayDeaths]}'></pie-chart>
+        <table>
+          <tr>
+            <th>Deaths</th>
+            <th>Today Deaths</th>
+          </tr>
+          <tr>
+            <td>{{country.deaths}}</td>
+            <td>{{country.todayDeaths}}</td>
+          </tr>
+        </table>
+      </div>
+      <div  class="pie-chart-container">
+        <h2 class="comparison-title">Cases</h2>
+        <pie-chart class="pie-chart" :data='{label: ["Cases","Today Cases"], value:[country.cases,country.todayCases]}'></pie-chart>
+        <table>
+          <tr>
+            <th>Cases</th>
+            <th>Today Cases</th>
+          </tr>
+          <tr>
+            <td>{{country.cases}}</td>
+            <td>{{country.todayCases}}</td>
+          </tr>
+        </table>
+      </div>
+      <div  class="pie-chart-container">
+        <h2 class="comparison-title">Recovered</h2>
+        <pie-chart class="pie-chart" :data='{label: ["Recovered","Today Recovered"], value:[country.recovered,country.todayRecovered]}'></pie-chart>
+        <table>
+          <tr>
+            <th>Recovered</th>
+            <th>Today Recovered</th>
+          </tr>
+          <tr>
+            <td>{{country.recovered}}</td>
+            <td>{{country.todayRecovered}}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+  html {
+    font-weight: 200;
+  }
   h2,p  {
     font-family: 'Times New Roman', Times, serif;
   }
-  html {
-    font-weight: 200;
+  .country {
+    // background-color: #ffd000;
+    text-align: center;
+  }
+  .country a {
+    color: #ffbb00;
+    font-size: 18px;
+  }
+  .country a:hover {
+    color: #ffd000;
   }
   .summary-container {
     margin: 5rem auto;
@@ -90,5 +148,32 @@ export default {
     font-size: 22px;
     font-weight: bold;
     color: #ffd000;
+  }
+  .pie-chart-container {
+    max-width: 50rem;
+    margin: 6rem auto;
+    box-shadow: 0 0 10px rgb(199, 199, 199);
+    border-radius: 8px;
+    background: linear-gradient(45deg,#ffd900,#BA00FF);
+    text-align: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    color: white;
+  }
+  .comparison-title {
+    padding-top: 1rem;
+    text-transform: uppercase;
+    flex: 0.5;
+  }
+  .pie-chart {
+    padding-bottom: 3rem;
+    width: 100%;
+  }
+  table {
+    width: 40%;
+    padding: 1.5rem;
+    margin-left: 2rem;
+    align-items: center;
   }
 </style>
