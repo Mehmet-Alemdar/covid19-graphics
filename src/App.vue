9e886b48e617f2fee556548177bfbd999a9651c7
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-03-02 12:23:14 +0300

    Make navigation bar responsive
    When this commit is applied, it makes the navigation bar responsive and improves the navigation bar's appearance.

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,23 @@ export default {
 <template>
   <div id="app">
     <div id="nav">
-      <h3 v-on:click="goToHomePage()">Covid 19 Graphics</h3>
-      <div v-if="countries.length" class="countries">
-        <form action="country">
-          <select name="q" onchange="this.form.submit()">
+      <h3 class="go-to-home" v-on:click="goToHomePage()">Covid 19 Graphics</h3>
+      <div class="bar">
+        <router-link class="bar-element router" to="/compare">Compare countries</router-link>
+        <div v-if="countries.length" class="countries">
+          <form action="country">
+            <select class="bar-element" name="q" onchange="this.form.submit()">
+              <option value="">Countries</option>
+              <option  v-for="country in countries">{{country}}</option>
+            </select>
+          </form>
+        </div>
+        <div v-else>
+          <select class="bar-element" name="q" onchange="this.form.submit()">
             <option value="">Countries</option>
-            <option  v-for="country in countries">{{country}}</option>
           </select>
-        </form>
+        </div>
+        <a class="bar-element router" href="https://en.wikipedia.org/wiki/Coronavirus" target="_blank">More About Coronavirus</a>
       </div>
     </div>
     <router-view/>
@@ -40,29 +49,56 @@ export default {
 body {
   margin: 0;
   padding: 0;
+  background-color: whitesmoke;
+  
 }
 #app {
-  color: #ffffff;
-  background-color: #FF5733;
   font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
-
 #nav {
-  padding: 30px;
+  padding: 1rem 1rem 0 1rem;
+  background: linear-gradient(90deg,#db2f08,#db0972);
+  color: #ffffff;
 }
-h3 {
+.go-to-home {
+  cursor: pointer;
+}
+.go-to-home:hover {
+  color:#FFC800;
+}
+.bar {
+  margin: 0 auto;
+  display: flex;
+  max-width: 90%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
+  gap: 2rem;
+  text-align: center;
+  align-items: center;
+}
+.bar-element {
+  font-size: 1.04rem;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: white;
+  text-decoration: none;
   cursor: pointer;
 }
 select {
-  width: 280px;
-  height: 50px;
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%);
-  border: 2px solid white;
-  background-color: #FF5733;
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  color: #ffffff;
+  height: 2rem;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: none;
+}
+.router:hover {
+  color:#FFC800;
+}
+@media screen and (max-width: 600px) {
+  .bar {
+    flex-direction: column;
+    gap: 1rem;
+    padding-bottom: 1rem;
+  }
 }
 </style>
